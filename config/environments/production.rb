3b90fff891c15ff09449e974dfd2e8d351ec33ec
DmitryKorobko <korobko.dmitry@gmail.com>
2018-08-31 12:09:03 +0300

    Configured mailer for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'frozen-meadow-62051.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => '37a79d5e93de5e',
+      :password => '7b3bb3dba02154',
+      :address => 'smtp.mailtrap.io',
+      :domain => 'smtp.mailtrap.io',
+      :port => '2525',
+      :authentication => :cram_md5
+  }
 end
